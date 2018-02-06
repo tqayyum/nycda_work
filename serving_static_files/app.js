e86a1c47bea0e5f14ede7a2c0a0f276e41d84ebe
Tahir Qayyum <qayyumtahir00@gmail.com>
2018-02-06 09:57:19 -0500

    Adding npm path

--- serving_static_files/app.js
+++ serving_static_files/app.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const logger = require('morgan');
-const exHandlebars = require('exHandlebars');
+//const exHandlebars = require('exHandlebars');
+const path = require('path');
 const app = express();
 
 app.use(express.static('public'));
+app.use('/static', express.static(path.join(__dirname, 'public', 'logo.png')));
 
 app.get('/', function(req,res) {
 	res.send('Is the image here');
 });
+
+
 
 app.listen(3000, function() {
 	console.log('Are you listening');
