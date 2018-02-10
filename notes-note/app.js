7c3d11b557c7a5e9eb859862da31ce06b464b8b1
Tahir Qayyum <qayyumtahir00@gmail.com>
2018-02-10 10:01:35 -0500

    Adding all files to git

--- notes-note/app.js
+++
@@ -1,9 +0,0 @@
-console.log('Starting App');
-
-const fs = require('fs');
-const os = require('os');
-
-var user = os.userInfo();
-
-
-fs.appendFileSync('greetings.txt', `Hello ${user.username} !`);
