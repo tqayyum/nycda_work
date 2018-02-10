db8313ade6e791f6062de6d54200c8c037751934
Tahir Qayyum <qayyumtahir00@gmail.com>
2018-02-09 23:48:30 -0500

    Adding helloWorld and notes-note

---
+++ notes-note/app.js
@@ -0,0 +1,9 @@
+console.log('Starting App');
+
+const fs = require('fs');
+const os = require('os');
+
+var user = os.userInfo();
+
+
+fs.appendFileSync('greetings.txt', `Hello ${user.username} !`);
