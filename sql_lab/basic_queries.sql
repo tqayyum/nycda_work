690b0332c9e3878e334477526fea05efc704bb93
Tahir Qayyum <qayyumtahir00@gmail.com>
2018-02-28 08:51:19 -0500

    Adding new files

---
+++ sql_lab/basic_queries.sql
@@ -0,0 +1,51 @@
+-- BASIC QUERIES
+-- Unless otherwise stated, all queries should return all columns
+
+-- Get all information about all authors
+SELECT * 
+FROM authors;
+
+-- Get just the name and birth year of all authors
+SELECT name, birth_year 
+FROM authors;
+
+-- Get all authors born in the USA
+SELECT *
+FROM authors 
+WHERE nationality = 'United States of America';
+
+-- Get all authors born in the 20th centruy or later
+SELECT name
+FROM authors
+WHERE birth_year 
+BETWEEN 1900 
+AND 2000;
+
+-- Get all books published on 1985
+SELECT publication_date
+FROM books
+WHERE publication_date = 1985;
+
+-- Get all books published before 1989
+SELECT publication_date
+FROM books
+WHERE publication_date < 1989;
+
+-- Get just the title of all books.
+SELECT title
+FROM books;
+
+-- Get just the year that 'A Dance with Dragons' was published
+  -- Cry when you realize how long it's been
+
+-- Get all books which have `the` somewhere in their title (hint, look up LIKE/ILIKE)
+
+-- Add yourself as an author
+
+-- Add two books that you'd like to write (you can hard-code your id as the author id)
+
+-- Update one of your books to have a new title
+
+-- Delete your books
+
+-- Delete your author entry
