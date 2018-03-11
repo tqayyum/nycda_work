46170bc484f7eb8f8cc387a8b046edf8eea6f88e
Tahir Qayyum <qayyumtahir00@gmail.com>
2018-03-10 22:52:36 -0500

    Adding the /peopleMergerApp file to git hub

--- peopleMergerApp/app.js
+++ peopleMergerApp/app.js
@@ -1,3 +1,17 @@
-const express = require('express');
-const looger = require('morgan');
+console.log('Are you there!');
+
 const fs = require('fs');
+const scrPath1 = ('/Users/tqayyum/nycda_work/peopleMergerApp/people1.json');
+const scrPath2 = ('/Users/tqayyum/nycda_work/peopleMergerApp/people2.json');
+const readMultipleFiles = require('read-multiple-files');
+
+fs.readFile(scrPath1, (err, data) => {
+	if (err) throw err;
+	let fileData = JSON.parse(data);
+	console.log(fileData);
+
+	fs.writeFile ('peopleComplete.txt',  data, (err) => {
+		if (err) throw err;
+
+	});
+});
