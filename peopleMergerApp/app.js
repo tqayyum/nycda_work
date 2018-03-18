71e2724584fee0744ca66c040b0b5bc1a378c1b7
Tahir Qayyum <qayyumtahir00@gmail.com>
2018-03-18 12:52:57 -0400

    Adding the correct file to git

--- peopleMergerApp/app.js
+++ peopleMergerApp/app.js
@@ -1,17 +1,18 @@
 console.log('Are you there!');
-
+//Required files
 const fs = require('fs');
-const scrPath1 = ('/Users/tqayyum/nycda_work/peopleMergerApp/people1.json');
-const scrPath2 = ('/Users/tqayyum/nycda_work/peopleMergerApp/people2.json');
-const readMultipleFiles = require('read-multiple-files');
 
-fs.readFile(scrPath1, (err, data) => {
-	if (err) throw err;
-	let fileData = JSON.parse(data);
-	console.log(fileData);
-
-	fs.writeFile ('peopleComplete.txt',  data, (err) => {
-		if (err) throw err;
-
+//Reading the first file and storing the data in the fData1 variable 
+fs.readFile('./pplFiles/people1.json', 'utf8', (err, content) => {
+	let fData1 = JSON.parse(content);
+	//Reading the second file and storing the data in the fData2 variable 
+	fs.readFile('./pplFiles/people2.json', 'utf8', (err, content) => {
+	let fData2 = JSON.parse(content);
+	//Combining the data to the varibable combo
+	const combo = (fData1.sort() + fData2.sort());
+		//Writing to the file
+		fs.writeFile ('peopleComplete.txt',  combo, (err) => {
+			if (err) throw err;
+		});
 	});
 });
